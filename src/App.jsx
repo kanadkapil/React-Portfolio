af202256397a86efc83ec2b20bb544ca7d078672
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-07-22 12:16:11 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-﻿import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+﻿import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ClickSpark from './components/ClickSpark'; // <-- Import here
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-between  text-base-content">
-      <Header className="" />
-      <main className="flex-1 p-4 mt-5">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
+    <ClickSpark
+      sparkColor="#facc15"      // example yellow
+      sparkSize={12}
+      sparkRadius={20}
+      sparkCount={10}
+      duration={500}
+      easing="ease-out"
+      extraScale={1.5}
+      
+    >
+      <div className="min-h-screen flex flex-col justify-between text-base-content">
+        <Header />
+        <main className="flex-1 p-4 mt-5">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </ClickSpark>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './styles/global.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
